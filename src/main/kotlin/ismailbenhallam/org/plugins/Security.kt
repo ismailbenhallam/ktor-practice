de23657cc522fd639e912681e8e1eb220ca008af
Ismaïl BENHALLAM <ismailben44@gmail.com>
2022-08-15 23:14:14 +0100

    🛂 UserHashedTableAuth

--- src/main/kotlin/ismailbenhallam/org/plugins/Security.kt
+++ src/main/kotlin/ismailbenhallam/org/plugins/Security.kt
@@ -3,20 +3,24 @@ package ismailbenhallam.org.plugins
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
-import io.ktor.server.auth.UserIdPrincipal
-import io.ktor.server.auth.UserPasswordCredential
+import io.ktor.server.auth.UserHashedTableAuth
 import io.ktor.server.auth.basic
+import io.ktor.util.getDigestFunction
 
-private val ADMIN = UserPasswordCredential("admin", "admin")
+private val DIGEST_FUNCTION = getDigestFunction("SHA-256") { "salt-function${it.length}" }
+private val HASHED_USER_TABLE = UserHashedTableAuth(
+    table = mapOf(
+        "admin" to DIGEST_FUNCTION("admin"),
+    ),
+    digester = DIGEST_FUNCTION
+)
 
 fun Application.configureSecurity() {
     install(Authentication) {
         basic("auth-basic") {
             realm = "Access Person admin API"
             validate { credential ->
-                if (credential == ADMIN)
-                    UserIdPrincipal("admin")
-                else null
+                HASHED_USER_TABLE.authenticate(credential)
             }
         }
     }
